0fab4434418b47f67f7304452d529f875e74cadf
bgassler <bgassler4@gmail.com>
2014-07-10 10:31:14 -0400

    dev-sprint2

--- chap6.py
+++ chap6.py
@@ -1,9 +1,58 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Brendan Gassler
 
 
 # Ex. 6.6
+def first(word):
+	return word[0]
+
+def last(word):
+	return word[-1]
+
+def middle(word):
+	return word[1:-1]
 
 
+"""1) If you call middle on a string of just 2 letters it 
+returns the empty string. Same if it is called with a
+string of one letter. Also the Same for the empty string"""
+
+#number 2
+def is_palindrome(word):
+	length = len(word)
+	if length == 1:
+		#print 'True'
+		return True
+	elif length == 2:
+		if last(word) == first(word):
+			#print 'True'
+			return True
+		else:
+			#print 'False'
+			return False
+	elif length >= 3 :
+		if last(word) == first(word):
+			is_palindrome(middle(word))
+		else:
+			#print 'False'
+			return False
+
+
+#print middle('hello')
+is_palindrome('abcdefggfedcba')
 
 # Ex 6.8
+def gcd(a, b):
+	if a > b:
+		r = a % b
+		gcd(b, r)
+		print r
+	elif b > a:
+		r = b % a
+		gcd(a, r)
+		print r
+	else:
+		print a
+		return a
+
+gcd(9, 15)
--- chap7.py
+++ chap7.py
@@ -3,7 +3,34 @@
 
 
 # Ex. 7.5
+import math
 
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		recursion  = factorial(n-1)
+		num = n * recursion
+		return num
 
+def estimate_pi():
+	total = 0
+	iterations = 0
+	k = 0
+	factor = 2 * math.sqrt(2)/9801
+	while True:
+		numer  = factorial(4*k) * (1103 + 26390*k)
+		denom = (factorial(k) ** 4) * (396 ** (4*k))
+		term = factor * numer / denom
+		total += term
+		iterations += 1
+
+		if abs(term) < 1e-15: break
+		k += 1
+	print "number of iterations: ", + iterations
+	return 1 / total
+
+print estimate_pi()
 
 # How many iterations does it take to converge?
+#it takes 3 iterations to converge
